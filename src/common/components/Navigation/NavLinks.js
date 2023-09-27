f47c9864f2aeae4f6121a606f8c6a5789c32bc27
naveenWebanav <naveen.saini@webanav.com>
2023-09-27 20:12:22 +0530

    updated NavLinks.js according to context info

--- src/common/components/Navigation/NavLinks.js
+++ src/common/components/Navigation/NavLinks.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React,{useContext} from 'react';
 
 import './NavLinks.css';
 import { NavLink } from 'react-router-dom';
 
+import { LoginContext } from '../context';
+
 const NavLinks = (props) => {
+
+    const loggedin = useContext(LoginContext);
+    
     return(
         <ul className="navlinks">
+            
             <li>
-                <NavLink to = "/" exact>Everyone</NavLink>
+                <NavLink to = "/" exact>
+                    Everyone
+                </NavLink>
             </li>
+            
+            {loggedin.isLoggedIn && (
             <li>
                 <NavLink to = "/u1/locations">My Locations</NavLink>
             </li>
+            )}
+
+            {loggedin.isLoggedIn && (
             <li>
                 <NavLink to = "/locations/new">Add Location</NavLink>
             </li>
+            )}
+
+            {!loggedin.isLoggedIn && (
             <li>
                 <NavLink to = "/login">Sign/Up</NavLink>
             </li>
+            )}
+
+            {loggedin.isLoggedIn && (
+                <button onClick={loggedin.logout}>Log Out</button>
+            )}
+
         </ul>
     );
 }
