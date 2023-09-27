cc893f6bc2aaa84a7887584f44ee4073d1d90be0
naveenWebanav <naveen.saini@webanav.com>
2023-09-27 20:14:50 +0530

    app.js

---
+++ backend/practice/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express(); // all the methods of express will come in app.
+
+app.listen(5000 , () => { // pc will act a server.
+    console.log();
+}); 
