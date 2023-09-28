e54574a4bed5d436467a8b235c4ee0163a5e5a45
nscspc <saininaveen933@gmail.com>
2023-09-28 17:11:42 +0530

    created app.js
    added nodemon and express for backend

--- backend/practice/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const app = express(); // all the methods of express will come in app.
-
-app.listen(5000 , () => { // pc will act a server.
-    console.log();
-}); 
