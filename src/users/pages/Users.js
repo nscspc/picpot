744de32eb9bd5e50204ba61bf14bd413cc7805ef
nscspc <saininaveen933@gmail.com>
2023-10-02 22:25:30 +0530

    Integrated get users api in Users.js

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,27 +1,65 @@
-import React from "react";
-import UsersList from "../components/UsersList";
+// import React from "react";
+// import UsersList from "../components/UsersList";
 
-const Users = () => {
+// const Users = () => {
 
-    const ALL_USERS =
-    [
-        {
-            id:"u1",
-            name:"XYZ",
-            pic:"https://picsum.photos/200",
-            numberoflocations:5,
-        },
-        {
-            id:"u2",
-            name:"MNO",
-            pic:"https://picsum.photos/200",
-            numberoflocations: 1,
-        },
-    ];
+//     const ALL_USERS =
+//     [
+//         {
+//             id:"u1",
+//             name:"XYZ",
+//             pic:"https://picsum.photos/200",
+//             numberoflocations:5,
+//         },
+//         {
+//             id:"u2",
+//             name:"MNO",
+//             pic:"https://picsum.photos/200",
+//             numberoflocations: 1,
+//         },
+//     ];
+
+//     return(
+//         <UsersList items = {ALL_USERS}/>
+//     );
+// }
+
+// export default Users;
+
+
+import React, { useState, useEffect} from "react";
+import UserList from "../components/UsersList";
+const Users =()=>{
+    
+    const [savedUsers, setSavedUsers]=useState();
+    const [error, setError]=useState();
+    
+    useEffect(()=> {
+        const sendRequest= async()=>{
+            try{
+                const response = await fetch("http://localhost:5000/api/users");
+                const responseData= await response.json();
+                if(!response.ok){
+                    throw new Error(responseData.message);
+                }
+                setSavedUsers(responseData.message);
+            }catch(err){
+                alert(err.message, ()=>{
+                    setError(null);
+                });
+                setError(err.message);
+            }
+        };
+        sendRequest();
+    }, []);
 
     return(
-        <UsersList items = {ALL_USERS}/>
+        <React.Fragment>
+            {
+                savedUsers && <UserList items={savedUsers} />
+            };
+        </React.Fragment>
     );
-}
+};
 
 export default Users;
